747cbcf6647ac827544bbcd878063baa845504a3
mwanikifelix-01 <f.mwaniki@alustudent.com>
2026-06-25 02:19:02 -0700

    errors

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@ def uppercase(str):
     for i in str:
         ascii_val = ord(i)
         if 97 <= ascii_val <= 122:
-            i = chr(ascii_val -32)
+            i = chr(ascii_val - 32)
         print("{}".format(i), end="")
     print("")
