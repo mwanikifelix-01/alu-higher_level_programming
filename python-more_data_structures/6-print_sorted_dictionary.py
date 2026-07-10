b90c696b7222120bc013a44e3fdbd12978a3b193
mwnaikifelix-01 <f.mwaniki@alustudent.com>
2026-07-10 18:19:46 +0200

    errors 3

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     sorts = sorted(a_dictionary.keys())
     for key in sorts:
-        print("{}: {}".format(i, a_dictionary[key]))
+        print("{}: {}".format(key, a_dictionary[key]))
