848d67f28d5b29a1adf2da48eefd31e6fb564568
mwnaikifelix-01 <f.mwaniki@alustudent.com>
2026-07-28 12:28:44 +0200

    error documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""This module defines a square that is empty"""
 
 class Square:
-    '''This module defines an empty square class'''
+    """This module defines an empty square class"""
