3f44d81de2fef8924360e1d5de04a185eb77ec66
mwnaikifelix-01 <f.mwaniki@alustudent.com>
2026-07-28 12:59:38 +0200

    Commits 2 files

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a square"""
+
+
+class Sqaure:
+    """This class initializes instance attribute"""
+    def __init__(self, size):
+        """This function actually initializes the instance"""
+        self.__size = size
---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module sets square"""
+
+class Square:
+    """A class function that defines functions"""
+    def __init__(self, size=0):
+        """checks size if it is an integer"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
